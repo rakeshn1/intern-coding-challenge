5c85a1f7a47cc8ebba99236754b5365ee47c3318
Rakesh Nagarajappa <78629375+rakeshn1@users.noreply.github.com>
2021-11-28 11:04:37 -0800

    Adding landing page to show the tickets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,16 @@
-import React, { Component } from 'react';
+import React from "react";
+import Main from "./Components/Main/Main"
+import Header from "./Components/Header/Header"
 
+const App = () => {
 
+  return (
+      <div>
+          <Header/>
+          <Main/>
+      </div>
+
+  );
+};
+
+export default App;
